a11d84b44dcaeea54fdaefd071f30d187e21f020
Boris Davydenko <brsdv@yandex.ru>
2021-09-24 18:37:58 +0300

    basis app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,115 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+
+* {
+    font-family: "Poppins", sans-serif;
+    box-sizing: border-box;
+    background-color: #141625;
+    padding: 0;
+    margin: 0;
 }
 
-#nav {
-  padding: 30px;
+button,
+.button {
+    cursor: pointer;
+    font-size: 12px;
+    color: #fff;
+    border: none;
+    border-radius: 30px;
+    padding: 16px 24px;
+    margin-right: 8px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.dark-purple {
+    background-color: #252945;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
+.red {
+    background-color: #ec5757;
+}
+
+.purple {
+    background-color: #7c5dfa;
+}
+
+.green {
+    background-color: #33d69f;
+}
+
+.orange {
+    background-color: #ff8f00;
+}
+
+// utility classes
+.flex {
+    display: flex;
+}
+
+.flex-column {
+    flex-direction: column;
+}
+
+.container {
+    width: 100%;
+    max-width: 850px;
+    padding: 40px 10px;
+    margin: 0 auto;
+
+    @media (min-width: 900px) {
+        padding-top: 72px;
     }
-  }
+}
+
+.nav-link {
+    text-decoration: none;
+    color: initial;
+}
+
+// Status Button Styling
+.status-button {
+    align-items: center;
+    font-size: 12px;
+    border-radius: 10px;
+    padding: 8px 30px;
+    margin-right: 30px;
+
+    &::before {
+        content: "";
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        margin-right: 8px;
+    }
+}
+
+.paid {
+    color: #33d69f;
+    background-color: rgba(51, 214, 160, 0.1);
+
+    &::before {
+        background-color: #33d69f;
+    }
+}
+
+.pending {
+    color: #ff8f00;
+    background-color: rgba(255, 145, 0, 0.1);
+
+    &::before {
+        background-color: #ff8f00;
+    }
+}
+
+.draft {
+    color: #dfe3fa;
+    background-color: rgba(223, 227, 250, 0.1);
+
+    &::before {
+        background-color: #dfe3fa;
+    }
 }
 </style>
